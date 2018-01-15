6d25d4484896456aa3c85ed11063e4ed3b3aa676
Sven Sabas <ssabas@gmail.com>
2018-01-15 11:19:58 +0000

    Added plotting and saving features to create_tree() function.

--- tree.py
+++ tree.py
@@ -21,10 +21,13 @@ def add_cut(current_branch, branch_length, branch_angle, side="positive"):
     return new_branch
 
 
-def create_tree(branch_length, branch_length_decay, tree_size, branch_angle):
+def create_tree(branch_length, branch_length_decay, tree_size, branch_angle,
+                to_plot='yes', to_save='yes', file_name='tree.png'):
 
     previous_level = [[0, branch_length, 0]]
-    plt.plot([0, 0], [0, branch_length])
+
+    if to_plot == 'yes':
+        plt.plot([0, 0], [0, branch_length])
 
     for i in range(tree_size):
         current_level = []
@@ -33,11 +36,15 @@ def create_tree(branch_length, branch_length_decay, tree_size, branch_angle):
             current_level.append(add_cut(branch, branch_length, branch_angle, side="negative"))
             current_level.append(add_cut(branch, branch_length, branch_angle, side="positive"))
 
-            plt.plot([branch[0], current_level[-2][0]], [branch[1], current_level[-2][1]])
-            plt.plot([branch[0], current_level[-1][0]], [branch[1], current_level[-1][1]])
+            if to_plot == 'yes':
+                plt.plot([branch[0], current_level[-2][0]], [branch[1], current_level[-2][1]])
+                plt.plot([branch[0], current_level[-1][0]], [branch[1], current_level[-1][1]])
 
         previous_level = current_level
         branch_length *= branch_length_decay
+
+    if to_save == 'yes':
+        plt.savefig(file_name)
 
 
 create_tree(config["branch_length"],
