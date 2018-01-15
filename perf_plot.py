2aef4c286ef4be931e3b32b1cce0584f110c5db1
Sven Sabas <ssabas@gmail.com>
2018-01-15 12:17:35 +0000

    Included option to use NumPy array based tree generation.

--- perf_plot.py
+++ perf_plot.py
@@ -5,16 +5,23 @@ from matplotlib.ticker import MaxNLocator
 
 
 def time_the_tree(max_layers=5, number_of_runs=100, number_of_repeats=3,
-                  to_plot='yes', to_save='no', file_name='perf_plot.png'):
+                  to_plot='yes', to_save='no', file_name='perf_plot.png',
+                  use_numpy='yes'):
 
     # Placeholders for time collection
     results = []
 
     for i in range(max_layers):
 
-        function_call = ('create_tree(branch_length = 6, branch_length_decay = 0.8, tree_size = {layer},'
-                         'branch_angle = 0.2, to_plot = "no", to_save="no")').format(layer=i)
-        setup_call = 'from tree import create_tree'
+        if use_numpy == 'yes':
+            function_call = ('create_tree_np(branch_length = 6, branch_length_decay = 0.8, tree_size = {layer},'
+                             'branch_angle = 0.2, to_plot = "no", to_save="no")').format(layer=i)
+            setup_call = 'from tree_np import create_tree_np'
+
+        else:
+            function_call = ('create_tree(branch_length = 6, branch_length_decay = 0.8, tree_size = {layer},'
+                             'branch_angle = 0.2, to_plot = "no", to_save="no")').format(layer=i)
+            setup_call = 'from tree import create_tree'
 
         t = timeit.Timer(function_call, setup_call)
         layer_time = min(t.repeat(number=number_of_runs, repeat=number_of_repeats))
@@ -51,10 +58,13 @@ if __name__ == "__main__":
                         help="Number of repeated experiments, default 3.")
     parser.add_argument('--to_save', '-s', type=str, required=False, default="yes",
                         help="To save the output in file named oerf_plot.png.")
+    parser.add_argument('--use_numpy', '-u', type=str, required=False, default="no",
+                        help="Toggle to use NumPy based arrays.")
     arguments = parser.parse_args()
 
     time_the_tree(max_layers=arguments.max_layers,
                   number_of_runs=arguments.number_of_runs,
                   number_of_repeats=arguments.number_of_repeats,
-                  to_save=arguments.to_save)
+                  to_save=arguments.to_save,
+                  use_numpy=arguments.use_numpy)
 
