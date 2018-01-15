82e0024e67088c28af338dad144d83777cf32cca
Sven Sabas <ssabas@gmail.com>
2018-01-15 10:00:30 +0000

    Identified constants/parameters and put them as separate global variables.

--- tree.py
+++ tree.py
@@ -1,22 +1,28 @@
 from math import sin, cos
 from matplotlib import pyplot as plt
 
+# Constants
+a = 5
+b = 0.2
+c = 0.6
 s = 1
-d = [[0,1,0]]
-plt.plot([0,0],[0,1])
 
-for i in range(5):
+
+d = [[0, 1, 0]]
+plt.plot([0, 0], [0, 1])
+
+for i in range(a):
     n = []
     for j in range(len(d)):
-        n.append([d[j][0]+s*sin(d[j][2]-0.2),
-                  d[j][1]+s*cos(d[j][2]-0.2),
-                  d[j][2]-0.2])
-        n.append([d[j][0]+s*sin(d[j][2]+0.2),
-                  d[j][1]+s*cos(d[j][2]+0.2),
-                  d[j][2]+0.2])
+        n.append([d[j][0]+s*sin(d[j][2]-b),
+                  d[j][1]+s*cos(d[j][2]-b),
+                  d[j][2]-b])
+        n.append([d[j][0]+s*sin(d[j][2]+b),
+                  d[j][1]+s*cos(d[j][2]+b),
+                  d[j][2]+b])
 
-        plt.plot([d[j][0], n[-2][0]],[d[j][1], n[-2][1]])
-        plt.plot([d[j][0], n[-1][0]],[d[j][1], n[-1][1]])
+        plt.plot([d[j][0], n[-2][0]], [d[j][1], n[-2][1]])
+        plt.plot([d[j][0], n[-1][0]], [d[j][1], n[-1][1]])
     d = n
-    s *= 0.6
+    s *= c
 plt.savefig('tree.png')
